4e813c7f16674dc231a7a682920f0b6c7b621265
Jeremie-Chauvel <Jeremie-Chauvel@users.noreply.github.com>
2019-04-18 10:32:49 +0200

    Pass from test to it convention for Jest tests

--- __tests__/pokemon.test.js
+++ __tests__/pokemon.test.js
@@ -2,14 +2,14 @@ import { mount } from '@vue/test-utils'
 import Pokemon from '../components/Pokemon'
 
 describe('Pokemon', () => {
-  test('is a Vue instance', () => {
+  it('Should be a Vue instance', () => {
     const wrapper = mount(Pokemon)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
 
 describe('convertHectogrammsToKilograms', () => {
-  test('return correct conversion value', () => {
+  it('Should return correct conversion value', () => {
     expect(Pokemon.methods.convertHectogrammsToKilograms(51)).toBe(5.1)
     expect(Pokemon.methods.convertHectogrammsToKilograms(60.9874)).toBe(6.1)
     expect(Pokemon.methods.convertHectogrammsToKilograms(0)).toBe(0)
